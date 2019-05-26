67fa04f1da075ec9d01d55818b53d96284a959e0
hjf <hjf@hjf.com.ar>
2019-05-26 00:50:54 -0300

    fix key

--- tpost.py
+++ tpost.py
@@ -13,7 +13,6 @@ bot = telegram.Bot(token=token)
 #print(bot.get_me())
 bot.send_photo(chat_id=chat_id, caption=image_text, photo=open(image, 'rb'),timeout=60)
 #from telegram.ext import Updater
-#updater = Updater(token='716850847:AAEFrKfYrv3iXFc7RilG3UqxueJA5yAvGZY', use_context=True)
 
 
 
