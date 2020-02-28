1ee6fa37c22c7f0c13f38d92a638e7e5d63a4c46
hjf <hjf@hjf.com.ar>
2020-02-28 20:22:45 -0300

    stuff

--- receive.sh
+++ receive.sh
@@ -60,16 +60,16 @@ exec 2<>${LOGFILE}.err
 #echo Device ${RTL_DEVICE} index ${RTL_DEVICE_INDEX}
 START_DATE=$(date '+%d-%m-%Y %H:%M')
 FOLDER_DATE="$(date +%Y)/$(date +%m)/$(date +%d)"
-#timeout "${6}" /usr/local/bin/rtl_fm  -d "${DEVICE}" -f "${2}"M -s 60k -g "${RECEIVE_GAIN}" -E wav -E deemp -F 9 - | /usr/bin/sox -t wav - ${NOAA_AUDIO}/audio/"${3}".wav rate 11025
-#echo timeout "${6}" /usr/local/bin/rtl_fm -f "${2}"M -s 60k -g 100 -E wav -E deemp -F 9 - 
-#echo timeout "${6}" /usr/bin/rtl_fm -g 50  -d "${RTL_DEVICE}" -f "${2}"M -s 60k -E wav -E deemp -F 9 - \| /usr/bin/sox -t raw -e signed -c 1 -b 16 -r 60000 - ${NOAA_AUDIO}/audio/"${3}".wav rate 11025
-#timeout "${6}" /usr/bin/rtl_fm -g 12  -d "${RTL_DEVICE}" -f "${2}"M -s 60k -E wav -E deemp -F 9 - | /usr/bin/sox -t raw -e signed -c 1 -b 16 -r 60000 - ${NOAA_AUDIO}/audio/"${3}".wav rate 11025
+#timeout "${6}" /usr/local/bin/rtl_fm  -d "${DEVICE}" -f "${2}"M -s 50k -g "${RECEIVE_GAIN}" -E wav -E deemp -F 9 - | /usr/bin/sox -t wav - ${NOAA_AUDIO}/audio/"${3}".wav rate 11025
+#echo timeout "${6}" /usr/local/bin/rtl_fm -f "${2}"M -s 50k -g 100 -E wav -E deemp -F 9 - 
+#echo timeout "${6}" /usr/bin/rtl_fm -g 50  -d "${RTL_DEVICE}" -f "${2}"M -s 50k -E wav -E deemp -F 9 - \| /usr/bin/sox -t raw -e signed -c 1 -b 16 -r 50000 - ${NOAA_AUDIO}/audio/"${3}".wav rate 11025
+#timeout "${6}" /usr/bin/rtl_fm -g 12  -d "${RTL_DEVICE}" -f "${2}"M -s 50k -E wav -E deemp -F 9 - | /usr/bin/sox -t raw -e signed -c 1 -b 16 -r 50000 - ${NOAA_AUDIO}/audio/"${3}".wav rate 11025
 /usr/sbin/uhubctl -l 1-1 -p 2 -a 0
 sleep 2
 /usr/sbin/uhubctl -l 1-1 -p 2 -a 1
 sleep 5
-echo timeout "${6}" /usr/bin/rtl_fm  -g "${RECEIVE_GAIN}"  -f "${2}"M -s 60k -E wav -E deemp -F 9 - | /usr/bin/sox -t raw -e signed -c 1 -b 16 -r 60000 - ${NOAA_AUDIO}/audio/"${3}".wav rate 11025
-timeout "${6}" /usr/bin/rtl_fm  -g "${RECEIVE_GAIN}"  -f "${2}"M -s 60k -E wav -E deemp -F 9 - | /usr/bin/sox -t raw -e signed -c 1 -b 16 -r 60000 - ${NOAA_AUDIO}/audio/"${3}".wav rate 11025
+echo timeout "${6}" /usr/bin/rtl_fm  -g "${RECEIVE_GAIN}"  -f "${2}"M -s 50k -E wav -E deemp -F 9 - | /usr/bin/sox -t raw -e signed -c 1 -b 16 -r 50000 - ${NOAA_AUDIO}/audio/"${3}".wav rate 11025
+timeout "${6}" /usr/bin/rtl_fm  -g "${RECEIVE_GAIN}"  -f "${2}"M -s 50k -E wav -E deemp -F 9 - | /usr/bin/sox -t raw -e signed -c 1 -b 16 -r 50000 - ${NOAA_AUDIO}/audio/"${3}".wav rate 11025
 sleep 10
 PASS_START=$(expr "$5" + 90)
 SUN_ELEV=$(python2 ${NOAA_HOME}/sun.py $PASS_START)
@@ -89,7 +89,7 @@ for i in $ENHANCEMENTS; do
 	/usr/bin/nice -n 10 /usr/local/bin/wxtoimg -o -m ${NOAA_AUDIO}/map/"${3}"-map.png -e $i ${NOAA_AUDIO}/audio/"${3}".wav ${NOAA_OUTPUT}/image/${FOLDER_DATE}/"${3}"-$i.jpg
 	/usr/bin/nice -n 10 /usr/bin/convert -quality 90 -format jpg ${NOAA_OUTPUT}/image/${FOLDER_DATE}/"${3}"-$i.jpg -undercolor black -fill yellow -pointsize 18 -annotate +20+20 "${1} $i ${START_DATE} MEL ${7}" ${NOAA_OUTPUT}/image/${FOLDER_DATE}/"${3}"-$i.jpg
 	if [ "${i}" = "MCIR-precip" ]; then
-		/usr/bin/python3 ${NOAA_HOME}/tpost.py ${TELEGRAM_TOKEN} ${TELEGRAM_CHAT_ID} ${NOAA_OUTPUT}/image/${FOLDER_DATE}/$3-MCIR-precip.jpg "MCIR Precip MEL: ${7}" &
+		/usr/bin/python3 ${NOAA_HOME}/tpost.py ${TELEGRAM_TOKEN} ${TELEGRAM_CHAT_ID} ${NOAA_OUTPUT}/image/${FOLDER_DATE}/$3-MCIR-precip.jpg "${1} MCIR Precip MEL: ${7}" &
 	fi
 #	/home/pi/tpost.py ${TELEGRAM_TOKEN} ${TELEGRAM_CHAT_ID} 
 #	/usr/bin/gdrive upload --parent 1gehY-0iYkNSkBU9RCDsSTexRaQ_ukN0A ${NOAA_OUTPUT}/image/${FOLDER_DATE}/"${3}"-$i.jpg
--- receive_meteor.sh
+++ receive_meteor.sh
@@ -83,30 +83,49 @@ rm -f /tmp/meteor_iq
 
 echo mkfifo /tmp/meteor_iq
 mkfifo /tmp/meteor_iq
-echo /usr/bin/meteor_demod -B -s 140000 /tmp/meteor_iq -o "${NOAA_AUDIO}/${3}.qpsk" 
-/usr/bin/meteor_demod -B -s 140000 /tmp/meteor_iq -o "${NOAA_AUDIO}/${3}.qpsk" &
-echo timeout ${6} /usr/bin/rtl_fm -M raw -s 140000 -f 137.1M -E dc -g "${RECEIVE_GAIN}" /tmp/meteor_iq
-timeout ${6} /usr/bin/rtl_fm -M raw -s 140000 -f 137.1M -E dc -g "${RECEIVE_GAIN}" /tmp/meteor_iq
+echo /usr/bin/meteor_demod -B -s 144k /tmp/meteor_iq -o "${NOAA_AUDIO}/${3}.qpsk" 
+nice -n 10 /usr/bin/meteor_demod -B -R 1000 -b 80 -s 144k /tmp/meteor_iq -o "${NOAA_AUDIO}/${3}.qpsk" &
+echo timeout ${6} /usr/bin/rtl_fm -M raw -s 144k -f "${2}"M -E dc -g "${RECEIVE_GAIN}" /tmp/meteor_iq
+#timeout ${6} /usr/bin/rtl_fm -M raw -s 144k -f "${2}"M -E dc -g "${RECEIVE_GAIN}" /tmp/meteor_iq
+timeout ${6} /usr/bin/rtl_fm -M raw -s 144k -f "${2}"M -g 25 -E dc /tmp/meteor_iq
 sleep 5
 echo rm -f /tmp/meteor_iq
 rm -f /tmp/meteor_iq
 
 echo /usr/bin/nice -n 10 /usr/bin/medet_arm "${NOAA_AUDIO}/${3}.qpsk" "${NOAA_OUTPUT}/meteor_raw/${FOLDER_DATE}/${3}" -cd
-/usr/bin/nice -n 10 /usr/bin/medet_arm "${NOAA_AUDIO}/${3}.qpsk" "${NOAA_OUTPUT}/meteor_raw/${FOLDER_DATE}/${3}" -cd
+/usr/bin/nice -n 10 /usr/bin/medet_arm "${NOAA_AUDIO}/${3}.qpsk" "${NOAA_OUTPUT}/meteor_raw/${FOLDER_DATE}/${3}" -cd -na
 echo rm "${NOAA_AUDIO}/${3}.qpsk"
 rm "${NOAA_AUDIO}/${3}.qpsk"
 echo /usr/bin/nice -n 10 /usr/bin/medet_arm "${NOAA_OUTPUT}/meteor_raw/${FOLDER_DATE}/${3}.dec" "${NOAA_AUDIO}/${3}_122" -r 65 -g 65 -b 64 -d
-/usr/bin/nice -n 10 /usr/bin/medet_arm "${NOAA_OUTPUT}/meteor_raw/${FOLDER_DATE}/${3}.dec" "${NOAA_AUDIO}/${3}_122" -r 65 -g 65 -b 64 -d
+/usr/bin/nice -n 10 /usr/bin/medet_arm "${NOAA_OUTPUT}/meteor_raw/${FOLDER_DATE}/${3}.dec" "${NOAA_AUDIO}/${3}_122" -r 65 -g 65 -b 64 -d -na
+echo /usr/bin/nice -n 10 /usr/bin/medet_arm "${NOAA_OUTPUT}/meteor_raw/${FOLDER_DATE}/${3}.dec" "${NOAA_AUDIO}/${3}_122" -r 65 -g 65 -b 64 -d
+/usr/bin/nice -n 10 /usr/bin/medet_arm "${NOAA_OUTPUT}/meteor_raw/${FOLDER_DATE}/${3}.dec" "${NOAA_AUDIO}/${3}_IR" -r 68 -g 68 -b 68 -d -na
 echo /home/pi/meteor_rectify/rectify.py "${NOAA_AUDIO}/${3}_122.bmp"
 /home/pi/meteor_rectify/rectify.py "${NOAA_AUDIO}/${3}_122.bmp"
-echo /usr/bin/convert -quality 90 -format jpg "${NOAA_AUDIO}/${3}_122-rectified.png" -undercolor black -fill yellow -pointsize 18 -annotate +20+20 "${1} $i ${START_DATE}" ${NOAA_OUTPUT}/image/${FOLDER_DATE}/"${3}".jpg
-/usr/bin/convert -quality 90 -format jpg "${NOAA_AUDIO}/${3}_122-rectified.png" -undercolor black -fill yellow -pointsize 18 -annotate +20+20 "${1} $i ${START_DATE}" ${NOAA_OUTPUT}/image/${FOLDER_DATE}/"${3}".jpg
+echo /home/pi/meteor_rectify/rectify.py "${NOAA_AUDIO}/${3}_IR.bmp"
+/home/pi/meteor_rectify/rectify.py "${NOAA_AUDIO}/${3}_IR.bmp"
+echo /usr/bin/convert -quality 90 -format jpg "${NOAA_AUDIO}/${3}_122-rectified.png" -undercolor black -fill yellow -pointsize 18 -annotate +20+20 "${1} $i ${START_DATE}" ${NOAA_OUTPUT}/image/${FOLDER_DATE}/"${3}_122".jpg
+/usr/bin/convert -quality 90 -format jpg "${NOAA_AUDIO}/${3}_122-rectified.png" -undercolor black -fill yellow -pointsize 18 -annotate +20+20 "${1} $i ${START_DATE}" ${NOAA_OUTPUT}/image/${FOLDER_DATE}/"${3}_122".jpg
+echo /usr/bin/convert -quality 90 -format jpg "${NOAA_AUDIO}/${3}_IR-rectified.png" -undercolor black -fill yellow -pointsize 18 -annotate +20+20 "${1} $i ${START_DATE}" ${NOAA_OUTPUT}/image/${FOLDER_DATE}/"${3}_IR".jpg
+/usr/bin/convert -quality 90 -format jpg "${NOAA_AUDIO}/${3}_IR-rectified.png" -undercolor black -fill yellow -pointsize 18 -annotate +20+20 "${1} $i ${START_DATE}" ${NOAA_OUTPUT}/image/${FOLDER_DATE}/"${3}".jpg
 echo rm "${NOAA_AUDIO}/${3}_122.bmp"
 rm "${NOAA_AUDIO}/${3}_122.bmp"
 echo rm "${NOAA_AUDIO}/${3}_122-rectified.png"
 rm "${NOAA_AUDIO}/${3}_122-rectified.png"
 
+echo rm "${NOAA_AUDIO}/${3}_IR.bmp"
+rm "${NOAA_AUDIO}/${3}_IR.bmp"
+echo rm "${NOAA_AUDIO}/${3}_IR-rectified.png"
+rm "${NOAA_AUDIO}/${3}_IR-rectified.png"
+
+if [ "${SUN_ELEV}" -gt "${SUN_MIN_ELEV}" ]; then
 echo /usr/bin/python3 ${NOAA_HOME}/tpost.py ${TELEGRAM_TOKEN} ${TELEGRAM_CHAT_ID} ${NOAA_OUTPUT}/image/${FOLDER_DATE}/"${3}".jpg "Meteor M2 False Color. MEL ${7}"
+/usr/bin/python3 ${NOAA_HOME}/tpost.py ${TELEGRAM_TOKEN} ${TELEGRAM_CHAT_ID} ${NOAA_OUTPUT}/image/${FOLDER_DATE}/"${3}_122".jpg "Meteor M2 False Color. MEL ${7}"
+else
+echo /usr/bin/python3 ${NOAA_HOME}/tpost.py ${TELEGRAM_TOKEN} ${TELEGRAM_CHAT_ID} ${NOAA_OUTPUT}/image/${FOLDER_DATE}/"${3}_IR".jpg "Meteor M2 IR MEL ${7}"
 /usr/bin/python3 ${NOAA_HOME}/tpost.py ${TELEGRAM_TOKEN} ${TELEGRAM_CHAT_ID} ${NOAA_OUTPUT}/image/${FOLDER_DATE}/"${3}".jpg "Meteor M2 False Color. MEL ${7}"
+
+fi
+
 
 #rm ${NOAA_AUDIO}/audio/*
